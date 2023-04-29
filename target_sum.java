794f4c967b9b5f2d8130892e435612af59d9a0e1
Shubham Ghodkhande <130497256+scopeofice@users.noreply.github.com>
2023-04-29 23:10:19 +0530

    Add files via upload

---
+++ target_sum.java
@@ -0,0 +1,17 @@
+package dfghj;
+
+public class target_sum {
+
+	public static void main(String[] args) {
+		int []arr= {1,2,3,4,5};
+		int target=5;
+		for(int i=0;i<arr.length;i++) {
+			for(int j=i+1;j<arr.length;j++) {
+			if(arr[j]==target-arr[i]) {
+				System.out.println("("+arr[i]+","+arr[j]+")");
+			}
+		}
+		}
+	}
+
+}
